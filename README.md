81129d14df06c8c79558dda28936dafd1f85337b
Adriano Cardoso <adrianofc4@hotmail.com>
2025-07-09 22:05:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Aprendendo Linguagens | Python
+
+Repositório do meu progresso como dev
+
+## 📚 Documentação 
+- [Documentação Python](https://docs.python.org/3/using/index.html)
---
+++ main.py
@@ -0,0 +1,30 @@
+#print ("Hello World")
+
+#Tipos de operadores
+
+"""
+soma = 5+5
+sub = 5-3
+mult = 3*3
+div = 3/3
+divint = 3//3 # '//' significa que ele vai me retornar apenas a parte inteira da divisão
+restdiv = 10%3 # retorna o resto da divisão
+potencia = 10**2
+print(soma, sub, mult, div)
+
+3+2.5
+"""
+
+soma = 10+3
+print (soma)
+
+10+3
+
+#Exercicio raio
+'''n = 3.14159
+raio = float(input('Insira o valor:'))
+
+area = n * (raio**2)
+
+print('A=%0.3f' %area)
+'''
