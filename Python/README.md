e6bda0c670771ec7b33de5baa985169167563647
Adriano Cardoso <adrianofc4@hotmail.com>
2026-03-02 21:43:11 -0300

    :docs: README atualizado com os novos aprendizados  + aula funções exercicios feitos

--- Python/README.md
+++ Python/README.md
@@ -9,3 +9,46 @@ Aqui armazeno toda a minha base de aprendizagem (com o tempo irei atualizando e 
  - Dados:
 
 [Teo Me Why](https://www.youtube.com/watch?v=OeKzVjiiRm4&list=PLvlkVRRKOYFSpRkqnR0p2A-eaVlpLnN3D)
+
+
+## Operadores Matématicos no Python:
+'''
+*Básicos:*
+"+" -> Adição,
+"-" -> Subtração,
+"/" -> Divisão (Float),
+"//" -> Divisão (Int),
+"%" -> Resto da divisão. Exemplo: % 2 == 0 Isso indica se o programa é par ou impar.,
+"*" -> Multiplicação, 
+"**" -> Exponenciação
+
+*Atribuição Matemática:*
++= -> Soma e atribui (x +=5 isso é igual a x+5)
+-= -> Substrai e atribui
+*= (Multiplica e atribui)
+/= (Divide e atribui)
+//= (Divide inteiro e atribui)
+%= (Calcula o módulo e atribui)
+**= (Eleva à potência e atribui)
+
+Nota: Se você precisar de operações matemáticas mais complexas (como raiz quadrada, seno, cosseno, logaritmos, etc.), o Python possui um módulo nativo chamado math. Você não usa um operador simples para eles, mas sim importa a biblioteca usando import math.
+'''
+
+##🤓Observações e Aprendizados:
+
+- Diferença entre listas e tuplas:
+    Basicamente as *Listas* trabalham com dados mutaveis ou seja dados que podem sofrer alterações no futuro, normalmente se utiliza [] possui diversos métodos que se podem trabalhar com ela, como: .append(), .remove().
+
+    Utilidade: Muito útil para trabalhar com dados homogeneos (dados do mesmo tipo) e dados que podem sofrer alteração no futuro.
+
+    Comum uso:
+    ¹ Um carrinho de compras de um e-commerce onde itens são adicionados e removidos.
+    ² Filas e Pilhas: Quando você tem uma lista de tarefas pedentes a serem feitas e precisa atualiza-las, conceito esse utilizado em um de meus projetos [Task Tracker CLI](https://github.com/adrianofcardoso/task-tracker-CLI).
+    ³ Iterações e Filtragem: Quando você quer ler um arquivo com dados extensos e salvar arquivos especificos.
+
+    Já as *Tuplas* são o oposto, é para dados imutaveis, ou seja não tem como você altera-lo, remover e etc, normalmente se utiliza (), tamanho fixo e normalmente utilizam menos memorias que as listas, funcionando de forma mais rápida. Como: informações sensiveis ou dados pessoais.
+
+    Utilidade: 
+    ¹ Quando queremos garrantir que certos dados não serão alterados por outra parte do codigo.
+    ² Quando uma função precisa retornar mais de um valor.
+    ³ Chaves de um dicionario ([10, 20] "Numero":)
--- Python/funcoes.py
+++ Python/funcoes.py
@@ -4,6 +4,11 @@ Resumo:
 Você tera def chamando a função onde você atribui o nome da função = f
 e dentro do parenteses você coloca o parametro que você quer passar para a função, nesse caso x.
 
+Ideia de função:
+Uma boa prática de código é que a função serve para contar somente oque ela é, é interessante que ela não receba inputs e etc
+como no exemplo de EhPar(), ela calcula somente se é par ou impar, ela não recebe o input() do usuário 
+
+
 Funcionamento do def:
 Ele atua mais ou menos como um script.
 
@@ -38,8 +43,78 @@ def juros(aporte: float, taxa: float, anos: int)->float:
     '''
     return aporte * (1 + taxa) ** anos
 
-juros(aporte = 1000, taxa = 0.13, anos = 2)
+resultado = juros(aporte = 1000, taxa = 0.13, anos = 2)
+print("Seu retorno financeiro é:", resultado)
 
-juros()
+#%% Função EhPar ou EhImpar
 
-#TERMINAR DE ASSISTIR A AULA (https://www.youtube.com/watch?v=JZlJ1otXBD8&list=PLvlkVRRKOYFSpRkqnR0p2A-eaVlpLnN3D&index=9)
+def EhPar (numero: int):
+    if numero % 2 == 0:
+        print("Eh par")
+    else:
+        print("Eh impar")
+
+numero = int(input("Insira o valor para verificar se é par: "))
+EhPar (numero)
+
+'''
+Ideia de função:
+Uma boa prática de código é que a função serve para contar somente oque ela é, é interessante que ela não receba inputs e etc
+como no exemplo de EhPar(), ela calcula somente se é par ou impar, ela não recebe o input() do usuário 
+
+Return: Serve para você armazenar algum valor e retornar isso para o usuario.
+Print: Serve para você eixbir um valor para o usuario
+'''
+#%%
+def EhPar (numero: int):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
+
+numero = int(input("Insira o valor para verificar se é par: "))
+EhPar (numero)
+
+resultado = EhPar(numero)
+
+print("O restuldao é:", numero, "e ele é:", resultado)
+
+#%%
+def soma (a: float, b: float)->float:
+    return a + b
+
+def media (a:float, b:float)->float:
+    return soma(a,b) / 2
+
+a = int(input("Insira o primeiro valor para ser somado: "))
+b = int(input("Insira o segundo valor para ser somado:"))
+
+print ("A media desses numeros eh: ", media(a,b))
+
+'''
+OBSERVAÇÕES:
+Argumentos opcionais sempre devem estar DEPOIS de argumentos obrigatorios.
+'''
+
+#TOPICO IMPORTANTE SOBRE *ARGS
+'''
+Em resumo é uma tupla que argamazena 0 ou infinitos valores a partir de outros elementos obrigatorios.
+ Isso pode me ajudar quando eu tiver que inserir elementos futuros 
+'''
+
+#%%
+def soma (a:float, b:float, *args)->float:
+    valores = [a, b, *args]  
+    return sum(valores)
+
+def media (a:float, b:float, *args)->float:
+    return soma(a, b, *args) / (len(args) + 2)
+
+a = float(input("Insira o a valor para ser somado: "))
+b = float(input("Insira o b valor para ser somado: "))
+c = float(input("Insira o c valor para ser somado: "))
+
+print("A media é: ", media(a,b,c))
+# %%
+
+#TERMINAR DE ASSISTIR A AULA (https://www.youtube.com/watch?v=JZlJ1otXBD8&list=PLvlkVRRKOYFSpRkqnR0p2A-eaVlpLnN3D&index=10)
